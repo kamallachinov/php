8583b8fde627c48a961a593386e55e6be6f87f92
kamallachinov <kamallachinov5@gmail.com>
2024-08-13 17:25:03 +0400

    validation updated to next level

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,34 +1,29 @@
 <?php
-
-$config = require ("../config.php");
+require("../Validator.php");
+$config = require("../config.php");
 $db = new Database($config['database']);
 
 
 $heading = "Create a note page";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = [];
-    if(strlen($_POST['body']) == 0){
-            $errors['body'] = 'Please enter a note body';
+
+    if (! Validator::string($_POST['body'],1,1000)) {
+        $errors['body'] = 'The  body cannot be more than 1000 characters and is required.';
     }
 
-    if(strlen($_POST['body']) > 1000){
-        $errors['body'] = 'The  body cannot be more than 1000 characters';
-    }
-
-    if(empty($errors)){
-        $db->query('INSERT INTO notes ( body, user_id) VALUES ( :body, :user_id)',[
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes ( body, user_id) VALUES ( :body, :user_id)', [
             'body' => $_POST['body'],
             'user_id' => 1
         ]);
     }
 
 
-
 }
 
 
-
-require ("../views/note-create.view.php")
+require("../views/note-create.view.php")
 
 ?>
